6f933a6805caf63ef6fa009183dd32c274c9532d
Stephen Lavett <slavett@uab.edu>
2019-04-22 20:16:54 -0500

    Add initial controller and views
    Added products controller and 3 associated views

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,20 @@
+class ProductsController < ApplicationController
+	#index action
+	def index
+
+	end
+
+	#new action
+	def new
+		@product = Product.new
+	end
+
+	def create 
+		
+	end
+
+	#list action
+	def list
+
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	 
+	#root page 
+	root 'products#index'
+
+	#Product Create page
+	get '/product/new' => 'products#new'
+
+	#Listing Page
+	get '/product/list' => 'products#list'
+
 end
+
+# For details on the DSL available within this file, see 
+#http://guides.rubyonrails.org/routing.html
